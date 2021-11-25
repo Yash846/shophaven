66323b9d6c0cc4e6f3a0df155981ce830793d149
Yash Sawlani <ys.sawlani846@gmail.com>
2021-11-25 13:58:57 +0530

    initial commit and file structure setup

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,40 @@
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">JACKETS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">SHOES</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">WOMENS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">MENS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
